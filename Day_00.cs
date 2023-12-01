146e9c1e1e6d34aac0193ccc5019c88957fda6ca
Jetinat0r <jetin8t0r@gmail.com>
2023-11-30 21:02:46 -0500

    Project Setup

---
+++ Day_00.cs
@@ -0,0 +1,20 @@
+﻿//Template Day class
+public class Day_00
+{
+    public void Main()
+    {
+        string[] lines = File.ReadAllLines("Day_00_Input.txt");
+
+        Day_00_1(lines);
+    }
+
+    void Day_00_1(string[] input)
+    {
+
+    }
+
+    void Day_00_2(string[] input)
+    {
+
+    }
+}
---
+++ Day_01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Day_01
+{
+    public void Main()
+    {
+        string[] lines = File.ReadAllLines("Day_01_Input.txt");
+
+        Day_01_1(lines);
+    }
+
+    public void Day_01_1(string[] lines)
+    {
+
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            Console.WriteLine(lines[i]);
+        }
+    }
+
+    public void Day_01_2(string[] lines)
+    {
+
+    }
+}
